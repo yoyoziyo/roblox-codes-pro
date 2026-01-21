c91aab87d51d4ceec37067e8485b2d7b3e5d7f05
yoyozi <41512132+yoyoziyo@users.noreply.github.com>
2026-01-21 14:59:37 -0300

    Remove filterGames function and update copyCode
    Removed the filterGames function and updated copyCode to use originalText.

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-function filterGames() {
-    const input = document.getElementById('gameSearch');
-    const filter = input.value.toLowerCase();
-    const grid = document.getElementById('gameGrid');
-    
-    if (grid) {
-        const cards = grid.getElementsByClassName('card');
-        for (let i = 0; i < cards.length; i++) {
-            let name = cards[i].querySelector('h3').innerText.toLowerCase();
-            
-            if (filter === "") {
-                cards[i].style.display = "none";
-            } else if (name.indexOf(filter) > -1) {
-                cards[i].style.display = "block";
-            } else {
-                cards[i].style.display = "none";
-            }
-        }
-    }
-}
-
 function copyCode(text, button) {
     navigator.clipboard.writeText(text).then(() => {
-        const oldText = button.innerText;
+        const originalText = button.innerText;
         button.innerText = "COPIADO!";
-        setTimeout(() => { button.innerText = oldText; }, 2000);
+        setTimeout(() => {
+            button.innerText = originalText;
+        }, 2000);
     });
 }
