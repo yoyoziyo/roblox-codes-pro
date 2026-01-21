8757cb46f76e7bb3b9156efca46b373cadc3f9bd
yoyozi <41512132+yoyoziyo@users.noreply.github.com>
2026-01-21 16:00:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+function filterGames() {
+    const input = document.getElementById('gameSearch');
+    const filter = input.value.toLowerCase();
+    const grid = document.getElementById('gameGrid');
+    
+    if (grid) {
+        const cards = grid.getElementsByClassName('card');
+        for (let i = 0; i < cards.length; i++) {
+            let name = cards[i].querySelector('h3').innerText.toLowerCase();
+            
+            if (filter === "") {
+                cards[i].style.display = "none";
+            } else if (name.indexOf(filter) > -1) {
+                cards[i].style.display = "block";
+            } else {
+                cards[i].style.display = "none";
+            }
+        }
+    }
+}
+
 function copyCode(text, button) {
     navigator.clipboard.writeText(text).then(() => {
-        const originalText = button.innerText;
+        const oldText = button.innerText;
         button.innerText = "COPIADO!";
-        setTimeout(() => {
-            button.innerText = originalText;
-        }, 2000);
+        setTimeout(() => { button.innerText = oldText; }, 2000);
     });
 }
